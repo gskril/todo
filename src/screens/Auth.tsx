813bbff94a2c3a52122e5f4961e825dac3e4c36d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-12-25 00:31:04 -0500

    Try to fix Google OAuth redirect

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -52,6 +52,9 @@ export default function Auth() {
 
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+        redirectTo: window.location.origin,
+      },
     })
 
     if (error) {
